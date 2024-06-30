4a353863ed59162e5747dea629e0b0bdb9f1317b
serxeweb <serxegraal@gmail.com>
2024-06-30 22:00:58 +0800

    Update send.php

--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['submit'])) {
         $name = $_POST['name'];
         $email = $_POST['email'];
         $message = $_POST['message'];
